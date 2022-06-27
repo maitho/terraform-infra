8faff1998e8d387f857d945e5aeaa079337af80c
David M.T <thigedavidmaitho@gmail.com>
2022-06-27 20:15:12 +0300

    define base eks resources

---
+++ README.md
@@ -0,0 +1,3 @@
+# DRY IaC with Terraform
+
+This is used with the (live-infra)[https://github.com/maitho/live-infra] containing terragrunt source to provion aws cloud resources.
---
+++ eks/vars.tf
@@ -0,0 +1,42 @@
+variable "env" {
+  type    = string
+  default = "eng"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "availability_zones_count" {
+  description = "The number of AZs."
+  type        = number
+  default     = 2
+}
+
+variable "project" {
+  description = "Name to be used on all the resources as identifier. e.g. Project name, Application name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_bits" {
+  description = "The number of subnet bits for the CIDR. For example, specifying a value 8 for this parameter will create a CIDR with a mask of /24."
+  type        = number
+  default     = 8
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default = {
+    "Project"     = "Jambo"
+    "Environment" = "Engineering"
+    "Owner"       = "David Maitho"
+  }
+}
